46c8ca479f09865939d469faa30f2614aec38102
James Grieve <jamesonrgrieve@gmail.com>
2024-08-30 12:46:36 -0600

    Fix auth routing;

--- app/user/[[...slug]]/page.tsx
+++ app/user/[[...slug]]/page.tsx
@@ -1,4 +1,14 @@
 import JRGAuthRouter from 'jrgcomponents/AuthRouter';
 export default function AuthRouter(props) {
-  return <JRGAuthRouter {...props} />;
+  return (
+    <JRGAuthRouter
+      {...props}
+      corePagesConfig={{
+        identify: {
+          path: '/',
+          heading: 'Please Sign Into Google and Allow Access to Your Documents',
+        },
+      }}
+    />
+  );
 }
