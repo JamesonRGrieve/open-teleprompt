b23a46b33bb6266d93b2604736e04de66254ce47
James Grieve <jamesonrgrieve@gmail.com>
2024-09-02 20:52:30 -0600

    Google Drive access;

--- app/user/[[...slug]]/page.tsx
+++ app/user/[[...slug]]/page.tsx
@@ -1,4 +1,5 @@
 import JRGAuthRouter from 'jrgcomponents/AuthRouter';
+import { Google } from '@mui/icons-material';
 export default function AuthRouter(props) {
   return (
     <JRGAuthRouter
@@ -7,6 +8,19 @@ export default function AuthRouter(props) {
         identify: {
           path: '/',
           heading: 'Please Sign Into Google and Allow Access to Your Documents',
+          props: {
+            oAuthOverrides: {
+              Google: {
+                client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+                scope: 'profile email https://www.googleapis.com/auth/drive.readonly',
+                uri: 'https://accounts.google.com/o/oauth2/v2/auth',
+                params: {
+                  access_type: 'offline',
+                },
+                icon: <Google />,
+              },
+            },
+          },
         },
       }}
     />
